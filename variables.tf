94a6de76b81ba0e1b4e0ca1d6ab9ee2216b5fc63
sowjanyatact <145376101+sowjanyatact@users.noreply.github.com>
2024-02-28 22:18:31 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "us-east-1" 
+       default     = "us-east-2" 
 }
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "mySep22Key" 
+    default     =  "newrefebkeypair" 
 }
 
 variable "instance_type" { 
